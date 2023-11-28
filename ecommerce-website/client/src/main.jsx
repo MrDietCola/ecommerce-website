746afebc7a7906025afccfeefac4ef94a97f3cf9
AJ Seidler <ajseidler0526@gmail.com>
2023-11-28 13:13:26 -0600

    set up main.jsx with router structure

--- ecommerce-website/client/src/main.jsx
+++ ecommerce-website/client/src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import Home from './components/homepageProducts.jsx';
+import CartPage from './components/cartPage.jsx';
+import Profile from './components/profile/profile.jsx';
+// import NotFound from './components/homepageProducts.jsx';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+        path: '/Profile/:id',
+        element: <Profile />
+      }, {
+        path: '/Cart/:id',
+        element: <CartPage />
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
